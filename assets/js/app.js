611bcf9dd0e5ceab78ea25048dccb96eced955d7
paperboatz <tiffwork25@gmail.com>
2017-03-24 17:11:45 -0400

    nav activeLinks, created journal.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,37 @@
+
+// ==== Navagation Drop down =====
+// instead of hide() to initally hide the nav
+// I used display: none in css, bc refreshes on page
+// reload flickered 
 var ulNestedList = $('.ul-nested ul');
 var liNestedList = $('.ul-nested li');
 var category = $('.ul-nested');
 
-	category.on('mouseenter', function(event){
-	  ulNestedList.slideDown(300);
-	});
+category.on('mouseenter', function(event){
+  ulNestedList.slideDown(300);
+});
 
-	category.on('mouseleave', function(event){
-	  ulNestedList.slideUp(300);
-	});
+category.on('mouseleave', function(event){
+  ulNestedList.slideUp(300);
+});
 
 
-// Email input against span bots
+// ==== Active Navagation =====
+// Found current path, grab last section of url
+// See if it match it to href on page 
+// if true, then add class, except on index page 
+var urlPath = window.location.pathname;
+var endPath = urlPath.substring(urlPath.lastIndexOf('/') + 1 );
+var link = $("a[href= '" + endPath + "']");
+var baseUrl = link[0].href;
+var contains = baseUrl.indexOf(urlPath);
+
+if (contains !== -1 && endPath !== 'index.html' ){
+  link.addClass('activelink');
+}
+
+
+// ==== Email Against Spam Bots =====
 $("<a href='mail" 
 	+ "to:tiffcodes" 
 	+ "@" 
